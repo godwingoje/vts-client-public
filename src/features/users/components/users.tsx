8535bb562bde05948a08f4378190eba281e56879
Godwin Goje <godwingoje173@gmail.com>
2026-08-28 10:45:13 +0100

    style: add ui improvements

--- src/features/users/components/users.tsx
+++ src/features/users/components/users.tsx
@@ -180,7 +180,7 @@ export default function UsersPage() {
           <Button
             size="small"
             type="primary"
-            className="flex h-7 gap-1 text-[13px]"
+            className="flex h-5 sm:h-6.5 gap-1 text-[13px]"
             onClick={() => setInviteModalOpen(true)}
           >
             <img
