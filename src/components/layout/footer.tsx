76887ed6b1f0bfc271351cf4bd5df4bf177afa4d
Godwin Goje <godwingoje173@gmail.com>
2026-09-02 08:49:28 +0100

    style: add ui improvements

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer({ className = "" }: { className?: string }) {
   return (
     <footer
-      className={`w-full bg-white ps-4 py-1 text-left text-[11px] text-slate-600 ${className}`}
+      className={`w-full bg-white ps-4 py-1 text-left text-[11px] text-slate-600 ${className} dark:bg-slate-900 dark:text-slate-400`}
     >
       Copyright {new Date().getFullYear()} Zoracom. All rights reserved.
     </footer>
